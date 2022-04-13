f1b9b9741c0be18c67664ae1ef9a560c5d81d70e
Ilya <i.oberemok777@gmail.com>
2022-04-13 11:43:23 +0300

    Add navbar to the base.html and create auth routes

---
+++ cafes/auth/routes.py
@@ -0,0 +1,23 @@
+# TODO: Create routes for the authentication functionality of the application
+
+# imports
+from flask import render_template, redirect, url_for, flash
+from cafes.auth import auth
+
+
+# login route
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+# register route
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+# logout route
+@auth.route("/logout")
+def logout():
+    pass
